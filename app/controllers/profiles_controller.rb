f7e4340c5cce5d8ce6523a6fef3823e693462878
instabrary <instabrary@3f4560be-fcb6-4516-b323-ae570ede3bb7>
2008-04-05 23:20:33 +0000

    Profiles were added. They are currently rather ugly, but it is there and mostly functions.
    git-svn-id: http://svn2.assembla.com/svn/instabrary/trunk@26 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,35 @@
-class ProfileController < ApplicationController
+class ProfilesController < ApplicationController
+  
+  def index
+    @user = User.find(session[:user])
+    @booksets = Bookset.find_all_by_user_id(@user)
+    
+    @statuses = BookStatus.find_all_by_user_id(@user)
+    
+    @own, @wants, @reading, @read, @toread, @part = [],[],[],[],[],[]
+    
+    @statuses.each { |s|
+      if(s.active == true)
+        case s.status.name
+          when "Own"
+            @own << s.book
+          when "Want"
+            @wants << s.book
+          when "Reading"
+            @reading << s.book
+          when "Read"
+            @read << s.book
+          when "To-read"
+            @toread << s.book
+          when "Read part"
+            @part << s.book 
+        end
+      end
+    }
+    
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
 end
