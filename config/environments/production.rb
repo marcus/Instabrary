26932b4aa73fb1d35a8556f0a2d52538e0ecdcc4
Marcus Vorwaller <marcus@vorwaller.net>
2010-03-05 20:03:53 -0800

    Undo all that stuff I did before

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,4 +16,3 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-Bundler.require_env RAILS_ENV
